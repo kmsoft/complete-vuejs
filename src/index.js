a2dca020d49b39afcd736fa537c1d39afd772a05
Sofiane Kara Mostefa <km.sofiane@gmail.com>
2021-07-14 15:07:54 +0200

    1.10 validation input step01

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const app = Vue.createApp({
   template: `
     <button v-on:click="increment">Increment</button>
     <p>{{ count }}</p>
+    <input
+      v-bind:value="value"
+      v-on:input="input"
+    />
+    <div class="red">{{ error }}</div>
     <div 
       v-for="number in numbers"
       v-bind:class="getClass(number)"
@@ -17,15 +22,26 @@ const app = Vue.createApp({
   data() {
     return {
       count: 0,
+      value: 'user',
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   },
   computed: {
     evenList() {
       return this.numbers.filter(num => this.isEven(num))
+    },
+
+    error(){
+      if (this.value.length < 5){
+        return 'must be > 5'
+      }
     }
   },
   methods: {
+    input($event){
+      this.value = $event.target.value
+    },
+
     increment() {
       this.count += 1
     },
